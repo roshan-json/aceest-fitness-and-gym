352cb06f0a7cc26a6df053a94247bb5452c56aa4
Roshan Jeason <roshanjeason@outlook.com>
2026-03-06 19:28:19 +0530

    Added dummy test file to verify CI

---
+++ test/test_app.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# Ensure src is importable during tests
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+import app
+
+def dummy_test():
+    assert 1 == 1
